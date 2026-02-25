fd936df361b001985252bb56b5fb28164e5fbc32
dave8git <dave8git@gmail.com>
2026-02-25 10:24:44 +0100

    bad commit

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,24 +1,51 @@
-const js = require("@eslint/js");
-const globals = require("globals");
+// const js = require("@eslint/js");
+// const globals = require("globals");
+
+// module.exports = [
+//   js.configs.recommended,
+//   {
+//     files: ["**/*.js"],
+//     languageOptions: {
+//       ecmaVersion: 2022,
+//       sourceType: "commonjs",
+//       globals: {
+//         ...globals.node,
+//         ...globals.browser
+//       }
+//     },
+//     rules: {
+//       "no-unused-vars": "warn",
+//       "no-console": "off",
+//       "semi": ["error", "always"],
+//       "quotes": ["error", "double"],
+//       "indent": ["error", 2]
+//     }
+//   }
+// ];
+
+// eslint.config.cjs
+
+const js = require('@eslint/js');
+const prettierPlugin = require('eslint-plugin-prettier');
+const prettierConfig = require('eslint-config-prettier');
 
 module.exports = [
   js.configs.recommended,
+
   {
-    files: ["**/*.js"],
     languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: "commonjs",
       globals: {
         ...globals.node,
-        ...globals.browser
-      }
+      },
+    },
+    plugins: {
+      prettier: prettierPlugin,
     },
     rules: {
-      "no-unused-vars": "warn",
-      "no-console": "off",
-      "semi": ["error", "always"],
-      "quotes": ["error", "double"],
-      "indent": ["error", 2]
-    }
-  }
+      'no-unused-vars': 'warn',
+      'prettier/prettier': 'error',
+    },
+  },
+
+  prettierConfig,
 ];
--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-const { app, BrowserWindow, ipcMain, dialog } = require("electron");
-const mm = require("music-metadata");
-const fs = require("fs").promises;
-const path = require("node:path");
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+const mm = require('music-metadata');
+const fs = require('fs').promises;
+const path = require('node:path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require("electron-squirrel-startup")) {
+if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
@@ -14,27 +14,27 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
+      preload: path.join(__dirname, 'preload.js'),
     },
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, "index.html"));
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
 };
 
-ipcMain.handle("get-files", async () => {
-  const { filePaths } = await dialog.showOpenDialog({ // The error is because dialog.showOpenDialog() does not return an array. It returns an object with two properties: cancled and filepaths
-    properties: ["openFile", "multiSelections"],
-    filters: [
-      { name: "Music", extensions: ["mp3"] },
-    ],
+ipcMain.handle('get-files', async () => {
+  const { filePaths } = await dialog.showOpenDialog({
+    // The error is because dialog.showOpenDialog() does not return an array. It returns an object with two properties: cancled and filepaths
+    properties: ['openFile', 'multiSelections'],
+    filters: [{ name: 'Music', extensions: ['mp3'] }],
   });
 
-  const metadata = await Promise.all( // await zatrzyma aż zostanie odczytane i zwrócone w tablicy metadane dla każdego pliku
-    filePaths.map(file => mm.parseFile(file))
+  const metadata = await Promise.all(
+    // await zatrzyma aż zostanie odczytane i zwrócone w tablicy metadane dla każdego pliku
+    filePaths.map((file) => mm.parseFile(file))
   );
 
   const filesData = await Promise.all(
@@ -42,13 +42,12 @@ ipcMain.handle("get-files", async () => {
       const stats = await fs.stat(file);
       return {
         metadata: metadata[index],
-
       };
     })
   );
 
   // console.log('filesData', filesData)
-  console.log("metadata", metadata);
+  console.log('metadata', metadata);
   return filesData;
 });
 
@@ -60,7 +59,7 @@ app.whenReady().then(() => {
 
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  app.on("activate", () => {
+  app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
@@ -70,8 +69,8 @@ app.whenReady().then(() => {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
